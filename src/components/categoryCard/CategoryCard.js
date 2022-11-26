2d913c35c2e7d7ab4293e93faf35d22983362c93
sauravBarua <myallmailofficial@gmail.com>
2022-11-26 10:35:11 +0100

    working on modal

--- src/components/categoryCard/CategoryCard.js
+++ src/components/categoryCard/CategoryCard.js
@@ -1,22 +1,47 @@
-import React from "react";
-import { Button, Card } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Card, Modal } from "react-bootstrap";
 
 const CategoryCard = ({ data }) => {
   const { img, title, price, mobile, location, category } = data;
+  const [show, setShow] = useState(false);
+
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+
   return (
     <div>
       <h1>Category {category} books </h1>
       <Card style={{ width: "18rem" }}>
         <Card.Img variant="top" src="holder.js/100px180" />
         <Card.Body>
+          <Card.Title> Seller's name </Card.Title>
+
           <Card.Title> {title} </Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Button variant="primary" onClick={handleShow}>
+            Book now
+          </Button>
         </Card.Body>
       </Card>
+      <div>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>{title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
     </div>
   );
 };
