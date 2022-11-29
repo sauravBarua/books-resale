79cf58fbcbd78b5a209bf904745dc790ed922151
sauravBarua <myallmailofficial@gmail.com>
2022-11-29 17:49:46 +0100

    added seller page

--- src/pages/seller/Seller.js
+++ src/pages/seller/Seller.js
@@ -11,34 +11,53 @@ const Seller = () => {
       return data;
     },
   });
+
+  const handleDelete = (id) => {
+    const proceed = window.confirm("Are you sure, you want to delete!");
+    if (proceed) {
+      fetch(`http://localhost:5000/users/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        });
+    }
+  };
   return (
     <div>
       <h1>seller</h1>
-      {users.map(
-        (user, i) =>
-          user.role === "seller" && (
-            <div key={i}>
-              <Table striped bordered hover>
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td> {i} </td>
-                    <td> {user.name} </td>
-                    <td> {user.email} </td>
-                    <td>@mdo</td>
-                  </tr>
-                </tbody>
-              </Table>
-            </div>
-          )
-      )}
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(
+            (user, i) =>
+              user.role === "seller" && (
+                <tr key={user._id}>
+                  <td> {i} </td>
+                  <td> {user.name} </td>
+                  <td> {user.email} </td>
+                  <td>
+                    {" "}
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => handleDelete(user._id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              )
+          )}
+        </tbody>
+      </Table>
     </div>
   );
 };
