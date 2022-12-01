8d734d3f716bb1f11bb42fee265f189057413f84
sauravBarua <myallmailofficial@gmail.com>
2022-12-01 09:36:57 +0100

    working on ad

--- src/pages/advertisement/Advertisement.js
+++ src/pages/advertisement/Advertisement.js
@@ -1,31 +1,46 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Button, Modal } from "react-bootstrap";
+import { Button, Card, Modal } from "react-bootstrap";
 
 const Advertisement = ({ id, show, handleClose }) => {
-  // useEffect(() => {
-  //   fetch(`http://localhost:5000/categories/${id}`)
-  //     .then((res) => res.json())
-  //     .then((data) => console.log(data));
-  // }, [id]);
+  const [data, setData] = useState("");
 
   useEffect(() => {
     axios.get(`http://localhost:5000/categories/${id}`).then((res) => {
-      console.log(res.data);
+      setData(res.data);
     });
   }, [id]);
+
+  const handleAd = () => {};
   return (
     <>
       <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title> </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        {/* <img
+            src={data.img}
+            alt=""
+            style={{
+              width: "300px",
+              height: "360px",
+              textAlign: "center",
+              padding: "15px",
+            }}
+          /> */}
+
+        <Modal.Body>
+          <input readOnly value={data.img} />
+          <input readOnly value={data.title} />
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button
+            variant="primary"
+            onClick={() => {
+              handleClose();
+              handleAd();
+            }}
+          >
             Save Changes
           </Button>
         </Modal.Footer>
